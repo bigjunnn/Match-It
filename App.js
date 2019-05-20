1454141186dba3c5e0034f97d7e46e5b4da924d3
Junwei <junwei.97@hotmail.com>
2019-05-20 14:56:17 +0800

    Basic Login Page

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+import Login from './app/components/Login'
+import Login2 from './app/components/Login2'
+
+export default class App extends Component {
+  render () {
+    return <Login />
+  }
+}
