2cb0254d72a566fe299778cb3d35c62b2eea9d27
Junwei <junwei.97@hotmail.com>
2019-07-19 23:43:32 +0800

    bug fix for android

--- android/app/src/main/java/com/matchit/MainApplication.java
+++ android/app/src/main/java/com/matchit/MainApplication.java
@@ -43,14 +43,6 @@ public class MainApplication extends Application implements ReactApplication {
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new RNCViewPagerPackage()
-        );
-    }
-
-    @Override
     protected String getJSMainModuleName() {
       return "index";
     }
